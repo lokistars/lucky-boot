e218c2fc633df4a0d78a39d4a30b0907b3c2ad01
暖阳 <532764597@qq.com>
2021-04-21 00:28:11 +0800

    gateway 网关配置补充

--- lucky/lucky-module-gateway/src/main/java/com/lucky/gateway/GatewayApplication.java
+++ lucky/lucky-module-gateway/src/main/java/com/lucky/gateway/GatewayApplication.java
@@ -2,8 +2,11 @@ package com.lucky.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class GatewayApplication {
 
     public static void main(String[] args) {
