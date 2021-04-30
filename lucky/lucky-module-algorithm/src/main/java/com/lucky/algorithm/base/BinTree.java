0f053a915e6d182a6561cad6f6d1f6bbfbe52c71
暖阳 <532764597@qq.com>
2021-05-01 02:16:22 +0800

    力扣刷题

--- lucky/lucky-module-algorithm/src/main/java/com/lucky/algorithm/base/BinTree.java
+++ lucky/lucky-module-algorithm/src/main/java/com/lucky/algorithm/base/BinTree.java
@@ -62,7 +62,8 @@ public class BinTree {
      */
     public static void print(int num){
         for(int i = 31; i>=0;i--){
-            System.out.print((num&(1<<i))== 0 ? "0":"1");
+            //System.out.print((num&(1<<i))== 0 ? "0":"1");
+            System.out.print((num>>i)& 1);
         }
         System.out.println( );
     }
