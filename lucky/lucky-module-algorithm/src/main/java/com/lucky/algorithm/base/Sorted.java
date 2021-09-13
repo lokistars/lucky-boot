1d0feb619f7290fcfe6e845b6783e01bf197b53e
暖阳 <532764597@qq.com>
2021-09-13 17:49:21 +0800

    增加快排

--- lucky/lucky-module-algorithm/src/main/java/com/lucky/algorithm/base/Sorted.java
+++ lucky/lucky-module-algorithm/src/main/java/com/lucky/algorithm/base/Sorted.java
@@ -204,6 +204,50 @@ public class Sorted {
      */
     static class RandomSort {
 
+        public static void main(String[] args) {
+            int[] arr = sortDate(7);
+            System.out.println(Arrays.toString(arr));
+            process(arr,0,arr.length-1);
+            System.out.println(Arrays.toString(arr));
+        }
+        private static void process(int[] arr,int L,int R){
+            if (L>=R){
+                return ;
+            }
+            swap(arr,L + (int) (Math.random() * (R-L+1)),R);
+            int[] area = partition(arr,L,R);
+            process(arr,L,area[0]-1);
+            process(arr,area[1]+1,R);
+        }
+
+        /**
+         * 小于目标数的在左边，大于目标数在右边
+         * @param arr
+         * @param L
+         * @param R 以R为目标数
+         * @return O(NlogN)
+         */
+        private static int[] partition(int[] arr,int L,int R){
+            if (L>R){ // 如果L大于R 就不是一个有效范围
+               return new int[]{-1,-1};
+            }
+            if (L==R){ // 只有一个数的情况下 L等于R
+                return new int[]{L,R};
+            }
+            int less = L-1; // 左边界
+            int more = R;   // 右边界
+            while (L < more){ //从L开始小于右边界
+                if (arr[L] == arr[R]){
+                    L++;
+                }else if(arr[L]<arr[R]){
+                    swap(arr,L++,++less);
+                }else{
+                    swap(arr,L,--more);
+                }
+            }
+            swap(arr,more,R);
+            return new int[]{less+1,more};
+        }
     }
 
     /**
