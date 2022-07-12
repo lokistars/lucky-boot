d3fdb50ff8e5a73f09d2f9489ebc76f836427be5
暖阳 <532764597@qq.com>
2022-07-12 21:06:14 +0800

    学习rabbitMq

---
+++ lucky/lucky-module-client/src/test/java/com/lucky/platform/rabbitMq/HelloWorld.java
@@ -0,0 +1,118 @@
+package com.lucky.platform.rabbitMq;
+
+import com.rabbitmq.client.*;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author: Loki
+ * @data: 2022-07-12 20:10
+ */
+public class HelloWorld {
+
+    /**
+     * hello world 使用的默认交换机,队列只能有一个消费者消费
+     * @throws Exception
+     */
+    @Test
+    public void helloPublish()throws Exception{
+        // 获取连接
+        final Connection connection = RabbitMqUtil.getConnection();
+        // 构建Channel
+        final Channel channel = connection.createChannel();
+        // 构建队列
+        channel.queueDeclare(RabbitMqUtil.HELLO_QUEUE,false,false,false,null);
+        // 发布消息
+        String message = "Hello World";
+        channel.basicPublish("",RabbitMqUtil.HELLO_QUEUE,null,message.getBytes());
+        System.out.println("消息发送成功");
+        //System.in.read();
+    }
+
+    @Test
+    public void helloConsumer() throws Exception{
+        final Connection connection = RabbitMqUtil.getConnection();
+        final Channel channel = connection.createChannel();
+        channel.queueDeclare(RabbitMqUtil.HELLO_QUEUE,false,false,false,null);
+        final DefaultConsumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("消费者获取消息："+new String(body,"UTF-8"));
+            }
+        };
+        channel.basicConsume(RabbitMqUtil.HELLO_QUEUE,true,consumer);
+        System.out.println("开始监听队列");
+        System.in.read();
+    }
+
+    /**
+     *  使用work queue
+     * @throws Exception
+     */
+    @Test
+    public void workPublish()throws Exception{
+        // 获取连接
+        final Connection connection = RabbitMqUtil.getConnection();
+        // 构建Channel
+        final Channel channel = connection.createChannel();
+        // 构建队列
+        channel.queueDeclare(RabbitMqUtil.WORK_QUEUE,false,false,false,null);
+        // 发布消息
+        for (int i = 0; i < 10; i++) {
+            String message = "Hello World"+i;
+            channel.basicPublish("",RabbitMqUtil.WORK_QUEUE,null,message.getBytes());
+        }
+        System.out.println("消息发送成功");
+        //System.in.read();
+    }
+
+    /**
+     * 使用 work queue 一个队列可以有多个消费者
+     * @throws Exception
+     */
+    @Test
+    public void workConsumer1() throws Exception{
+        final Connection connection = RabbitMqUtil.getConnection();
+        final Channel channel = connection.createChannel(1);
+        channel.queueDeclare(RabbitMqUtil.WORK_QUEUE,false,false,false,null);
+        // 设置消息的流控,一次获取多少条消息
+        channel.basicQos(1);
+        final DefaultConsumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("消费者一号获取消息："+new String(body,"UTF-8"));
+                // 手动ack,告诉rabbitmq已经消费了
+                channel.basicAck(envelope.getDeliveryTag(),false);
+            }
+        };
+        // false 关闭自动ack,默认情况下队列会以轮询的方式交给不同的消费者消费
+        channel.basicConsume(RabbitMqUtil.WORK_QUEUE,false,consumer);
+        System.out.println("开始监听队列");
+        System.in.read();
+    }
+
+    @Test
+    public void workConsumer2() throws Exception{
+        final Connection connection = RabbitMqUtil.getConnection();
+        final Channel channel = connection.createChannel();
+        channel.queueDeclare(RabbitMqUtil.WORK_QUEUE,false,false,false,null);
+        final DefaultConsumer consumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                System.out.println("消费者二号获取消息："+new String(body,"UTF-8"));
+            }
+        };
+        channel.basicConsume(RabbitMqUtil.WORK_QUEUE,true,consumer);
+        System.out.println("开始监听队列");
+        System.in.read();
+    }
+
+    /**
+     * publish/subscribe 发布订阅模式
+     */
+    @Test
+    public void publish(){
+
+    }
+}
---
+++ lucky/lucky-module-client/src/test/java/com/lucky/platform/rabbitMq/RabbitMqUtil.java
@@ -0,0 +1,36 @@
+package com.lucky.platform.rabbitMq;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+/**
+ * @author: Loki
+ * @data: 2022-07-12 20:31
+ */
+public class RabbitMqUtil {
+    private static final String RABBITMQ_HOST = "119.91.148.176";
+    private static final Integer RABBITMQ_PORT = 5672;
+    private static final String RABBITMQ_USERNAME = "lucky";
+    private static final String RABBITMQ_PASSWORD = "lucky";
+
+    public static final String HELLO_QUEUE = "hello";
+    public static final String WORK_QUEUE = "work";
+    public static final String PUBSUB_QUEUE = "pubsub";
+    public static final String ROUTING_QUEUE = "routing";
+    public static final String TOPIC_QUEUE = "topic";
+    public static final String RPC_QUEUE = "rpc";
+
+    public static Connection getConnection() throws Exception{
+        // 创建connection工厂
+        final ConnectionFactory factory = new ConnectionFactory();
+        // 设置RabbitMq连接信息
+        factory.setHost(RABBITMQ_HOST);
+        factory.setPort(RABBITMQ_PORT);
+        factory.setUsername(RABBITMQ_USERNAME);
+        factory.setPassword(RABBITMQ_PASSWORD);
+        factory.setVirtualHost("/");
+        // 返回连接对象
+        return factory.newConnection();
+    }
+
+}
