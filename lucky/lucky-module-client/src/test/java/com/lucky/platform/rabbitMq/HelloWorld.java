e83eb18f17fd92874576e5651817caa388dd1464
暖阳 <532764597@qq.com>
2022-08-04 17:59:34 +0800

    rabbitMq demo

--- lucky/lucky-module-client/src/test/java/com/lucky/platform/rabbitMq/HelloWorld.java
+++ lucky/lucky-module-client/src/test/java/com/lucky/platform/rabbitMq/HelloWorld.java
@@ -4,6 +4,8 @@ import com.rabbitmq.client.*;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author: Loki
@@ -12,6 +14,23 @@ import java.io.IOException;
 public class HelloWorld {
 
     /**
+     * queue: 队列名
+     * durable: 是否持久化,true 持久化
+     * exclusive: 是否排它,true 排它是基于连接可见,该队列仅对首次声明它的连接可见,并在连接断开后自动删除
+     * autoDelete: 是否自动删除,true 自动删除最后一个消息消费后队列是否被自动删除
+     * arguments:
+     *  message-ttl:发布到队列的消息在被丢弃之前可以存活多长时间（毫秒）
+     *  x-expires:队列在被自动删除之前可以未使用多长时间（毫秒）
+     *  x-max-length:队列在开始从其头部删除之前可以包含多少（就绪）消息
+     *  x-max-length-bytes:队列在开始从其头部掉落之前可以包含的就绪消息的总正文大小。
+     *  x-overflow:设置队列溢出行为。这决定了当达到队列的最大长度时， reject-publish-dlx（拒绝发送消息到死信交换器）
+     *  drop-head（删除queue头部的消息）,reject-publish（最近发来的消息将被丢弃）,
+     *  x-dead-letter-exchange:消息被拒绝或过期时将重新发布到的交换的交换的可选名称。
+     *  x-dead-letter-routing-key:当邮件是死信时使用的可选替换路由键。如果未设置，则将使用邮件的原始路由密钥
+     *  x-single-active-consumer:如果设置，请确保一次只有一个使用者从队列中消耗，并在活动使用者被取消或死亡时故障转移到另一个注册使用者
+     *  x-max-priority:队列要支持的最大优先级数;如果未设置，队列将不支持消息优先级。
+     *  x-queue-mode:将队列设置为懒惰模式，在磁盘上保留尽可能多的消息以减少RAM使用量;如果未设置，队列将保留内存中的缓存，以尽可能快地传递消息。
+     *  x-queue-master-locator:将队列设置为主位置模式，确定在节点群集上声明队列主服务器时所依据的规则。
      * hello world 使用的默认交换机,队列只能有一个消费者消费
      * @throws Exception
      */
@@ -21,13 +40,19 @@ public class HelloWorld {
         final Connection connection = RabbitMqUtil.getConnection();
         // 构建Channel
         final Channel channel = connection.createChannel();
-        // 构建队列
-        channel.queueDeclare(RabbitMqUtil.HELLO_QUEUE,false,false,false,null);
+        // 构建队列,设置过期时间
+        Map<String,Object> map = new HashMap<>(16);
+        map.put("x-message-ttl",10000);
+        map.put("x-max-length",3);
+        channel.queueDeclare(RabbitMqUtil.HELLO_QUEUE,false,false,false,map);
         // 发布消息
         String message = "Hello World";
-        channel.basicPublish("",RabbitMqUtil.HELLO_QUEUE,null,message.getBytes());
+        AMQP.BasicProperties.Builder basic = new AMQP.BasicProperties().builder();
+        basic.expiration("5000");
+        basic.deliveryMode(1);  //设置消息持久化, 1:非持久化、2:持久化
+        channel.basicPublish("",RabbitMqUtil.HELLO_QUEUE,basic.build(),message.getBytes());
         System.out.println("消息发送成功");
-        //System.in.read();
+        System.in.read();
     }
 
     @Test
