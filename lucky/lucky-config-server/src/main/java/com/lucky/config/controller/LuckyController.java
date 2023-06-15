b7a178981fe89ba1de8a22b0fee21e2c400e76b9
Loki <nuany997@gmail.com>
2023-06-15 20:54:23 +0800

    删除不必要的服务

---
+++ lucky/lucky-config-server/src/main/java/com/lucky/config/controller/LuckyController.java
@@ -0,0 +1,17 @@
+package com.lucky.config.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: lucky
+ * @description: lucky
+ * @author: Loki
+ * @data: 2023-06-15 20:50
+ **/
+@RestController
+@RequestMapping("/lucky")
+public class LuckyController {
+
+
+}
