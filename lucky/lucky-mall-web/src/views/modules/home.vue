e7da6544c62ad8a4ced6820a4a3b5de5eabbe734
暖阳 <532764597@qq.com>
2023-08-08 18:36:06 +0800

    新增mall-web项目

---
+++ lucky/lucky-mall-web/src/views/modules/home.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-card shadow="never" class="aui-card--fill">
+    <div class="mod-home">
+      <h3>项目介绍</h3>
+
+    </div>
+  </el-card>
+</template>
+
+<style>
+.mod-home {
+  line-height: 1.5;
+}
+</style>
